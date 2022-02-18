6ddbcc85f6afa307e7a43efd4eaaa0c4262a96ec
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-18 23:31:57 +0530

    [Guru] Modified to View person by city or state across addressbook -UC9

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -22,7 +22,7 @@ namespace AddressBookSystem
             while (true)
             {
                 Console.WriteLine("Please choose an option from the below list");
-                Console.WriteLine("\n1. Add New Address Book \n2. Add New Contact \n3. View Contacts \n4. View Contact by Person \n5. Edit Contact \n6. Delete Contact \n7. View all AddressBooks \n8. Switch AddressBook \n9. Search Person By City \n10.Exit\n");
+                Console.WriteLine("\n1. Add New Address Book \n2. Add New Contact \n3. View Contacts \n4. View Contact by Person \n5. Edit Contact \n6. Delete Contact \n7. View all AddressBooks \n8. Switch AddressBook \n9. Search Person By City or State \n10.View Person By City or State \n11.Exit\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -80,6 +80,11 @@ namespace AddressBookSystem
                         addressBook.SearchPersonByCityOrState(userData);
                         break;
                     case 10:
+                        Console.WriteLine("Enter the city or state to View person by city or state across addressbook: ");
+                        string cityOrState = Console.ReadLine();
+                        addressBook.SearchPersonByCityOrState(cityOrState);
+                        break;
+                    case 11:
                         Environment.Exit(0);
                         break;
                     default:
